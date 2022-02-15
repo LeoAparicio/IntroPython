f03fd9cde65f0f092b968afa7791aae45b880e62
LeoAparicio <nullservicios@gmail.com>
2022-02-14 23:48:13 -0600

    if structure

---
+++ Modulo 3/documentation.py
@@ -0,0 +1,9 @@
+# Condicional if
+#Tip de práctica 1: Intenta ejecutarlo en un notebook
+a = 97
+b = 55
+
+#Test expression /  espresión de prueba
+if a < b:
+    #statement to be run / instrucción a ejecutar
+    print(b)
